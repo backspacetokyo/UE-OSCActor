72222dc11e2177d95179421384bcab438ceb82e8
Satoru Higa <satoruhiga@gmail.com>
2022-12-09 02:58:25 +0900

    Add Project Settings section for IP and port

--- Source/OSCActor/Private/OSCActorModule.cpp
+++ Source/OSCActor/Private/OSCActorModule.cpp
@@ -1,18 +1,38 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "OSCActorModule.h"
+#include "ISettingsModule.h"
+#include "OSCActorSubsystem.h"
 
 #define LOCTEXT_NAMESPACE "FOSCActorModule"
 
 void FOSCActorModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings");
+	if (SettingsModule != nullptr)
+	{
+		SettingsModule->RegisterSettings(
+			"Project",
+			"Plugins",
+			"OSCActor",
+			FText::FromString(TEXT("OSCActor")),
+			FText::FromString(TEXT("")),
+			GetMutableDefault<UOSCActorSettings>()
+		);
+	}
 }
 
 void FOSCActorModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+	ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings");
+	if (SettingsModule != nullptr)
+	{
+		SettingsModule->UnregisterSettings(
+			"Project",
+			"Plugins",
+			"OSCActor"
+		);
+	}
 }
 
 #undef LOCTEXT_NAMESPACE
